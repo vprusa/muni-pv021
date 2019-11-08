f6dce4da21eeb04fda723e166f740f860d7c6fea
Vojtěch Průša <prusa.vojtech@gmail.com>
2019-11-08 17:01:50 +0100

    Added logger

--- src/main/java/cz/muni/fi/pv021/Application.java
+++ src/main/java/cz/muni/fi/pv021/Application.java
@@ -1,7 +1,16 @@
 package cz.muni.fi.pv021;
 
+import org.apache.log4j.Logger;
+
+/**
+ * This class main runner
+ *
+ * @author <a href="mailto:prusa.vojtech@email.com">Vojtech Prusa</a>
+ */
 public class Application {
+    static final Logger log = Logger.getLogger(Application.class);
+
     public static void main(String[] args){
-    System.out.println("Hello, World");
-  }
+        log.info("Hello, World");
+    }
 }
