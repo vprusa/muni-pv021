305315235482768be9f3a966f562d2216afa1d1b
Vojtěch Průša <prusa.vojtech@gmail.com>
2019-11-19 12:30:56 +0100

    Refactored structures, removed unnecessary.

--- src/main/java/cz/muni/fi/pv021/Application.java
+++ src/main/java/cz/muni/fi/pv021/Application.java
@@ -1,13 +1,11 @@
 package cz.muni.fi.pv021;
 
-import cz.muni.fi.pv021.model.Mlp;
-import cz.muni.fi.pv021.utils.DataReader;
+import cz.muni.fi.pv021.logic.NeuralNetwork;
+import cz.muni.fi.pv021.logic.NeuralNetworkFactory;
+import cz.muni.fi.pv021.model.Settings;
 import cz.muni.fi.pv021.utils.Utils;
 
 import java.util.logging.Logger;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 /**
  * Run with
@@ -16,72 +14,13 @@ import java.io.IOException;
  * @author <a href="mailto:34507957+czFIRE@users.noreply.github.com">Patrik Kadlec</a>
  */
 public class Application {
-    static final Logger log = Logger.getLogger(Application.class.getSimpleName());
 
     public static void main(String[] args){
-        log.info("Hello, World");
-        log.info("Seed: " + Utils.getSeed());
-        //new Test();
+        Settings settings = new Settings();
 
-        Utils.setSeed(138);   //change for better results
-        log.info("Seed: " + Utils.getSeed());
-
-        int epochs = 16;
-        double learningRate = 0.05;
-        double momentum = 0.8;
-        int []architecture = new int[] {784, 128, 10};
-        int miniBatchSize = 200;
-        /* Data provided to us by the teacher */
-        String trainData = "D:\\Java\\JavaNeuralNetwork\\MNIST_DATA\\mnist_train_vectors.csv";
-        String trainLabels = "D:\\Java\\JavaNeuralNetwork\\MNIST_DATA\\mnist_train_labels.csv";
-        String testData = "D:\\Java\\JavaNeuralNetwork\\MNIST_DATA\\mnist_test_vectors.csv";
-        String testLabels = "D:\\Java\\JavaNeuralNetwork\\MNIST_DATA\\mnist_test_labels.csv";
-
-        BufferedReader tData = null, tLabels = null, control = null;
-        try {
-            tData = new BufferedReader(new FileReader(trainData));
-            tLabels = new BufferedReader(new FileReader(trainLabels));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assert tData != null;
-        assert tLabels != null;
-
-        int [][][] batches = new int[Utils.dataSetLength/miniBatchSize][DataReader.dataPerLine][miniBatchSize];
-        int [][][] labels = new int[Utils.dataSetLength/miniBatchSize][DataReader.dataClasses][miniBatchSize];
-
-        DataReader.readData(tData, batches, DataReader.dataPerLine, miniBatchSize, Utils.dataSetLength);
-        DataReader.readData(tLabels, labels, 1, miniBatchSize, Utils.dataSetLength);
-
-        Mlp mlp = new Mlp(architecture, learningRate, miniBatchSize, momentum);
-
-        for (int epoch = 0; epoch < epochs; epoch++) {
-            for (int i = 0; i < Utils.dataSetLength/miniBatchSize; i++) {
-                mlp.evaluate(batches[i]);
-                mlp.momentumLayer3BackProp(batches[i], labels[i]);
-                if (i % 10 == 0) log.info("epoch: " + (epoch + 1) + " iteration: " + i);
-            }
-        }
-        log.info("done learning");
-
-        try {
-            control = new BufferedReader(new FileReader(testData));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assert control != null;
-
-        int [][][] controlTest = new int[Utils.testSetLength/miniBatchSize][DataReader.dataPerLine][miniBatchSize];
-        DataReader.readData(control, controlTest, DataReader.dataPerLine, miniBatchSize, Utils.testSetLength);
-
-        double [][][] answers = new double [Utils.testSetLength/miniBatchSize][miniBatchSize][DataReader.dataClasses];
-        for (int i = 0; i < Utils.testSetLength/miniBatchSize; i++) {
-            mlp.evaluate(controlTest[i]);
-            answers[i] = Utils.transposeMat(mlp.activations.get(1));
-        }
-
-        DataReader.write("D:\\Java\\JavaNeuralNetwork\\evaluator\\actualTestPredictions", answers);
+        NeuralNetwork nn = NeuralNetworkFactory.create(settings);
+        nn.learn();
+        nn.recognize();
     }
+
 }
---
+++ src/main/java/cz/muni/fi/pv021/model/Settings.java
@@ -0,0 +1,86 @@
+package cz.muni.fi.pv021.model;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * This class contains settings
+ *
+ * Settings are set of variables that can be passed via commandline or set as static
+ *
+ * For documentation check
+ *
+ * @see <a href="http://jcommander.org/">http://jcommander.org/</a>
+ *
+ * @author <a href="mailto:prusa.vojtech@email.com">Vojtech Prusa</a>
+ */
+public class Settings {
+
+    @Parameter(names = "-architecture")
+    public static int[] architecture = new int[] {784, 128, 10};;
+
+    public static int layers(){
+        return architecture != null ? architecture.length-1 : 0;
+    }
+
+    @Parameter(names = "-learningRate")
+    public static double learningRate = 0.05;
+
+    @Parameter(names = "-minBatchSize")
+    public static int miniBatchSize = 200;
+
+    @Parameter(names = "-momentum")
+    public static double momentum = 0.8;
+
+    @Parameter(names = "-epochs")
+    public static int epochs = 16;
+
+    //public static String resourcesDir = "D:\\Java\\JavaNeuralNetwork\\MNIST_DATA\\mnist_train_vectors.csv";
+    @Parameter(names = "-resourcesDir")
+    public static String resourcesDir = "./MNIST_DATA";
+
+    @Parameter(names = "-answers")
+    public static String answers = "evaluator/actualTestPredictions";
+
+    /* Data provided to us by the teacher */
+    @Parameter(names = "-trainData")
+    public static String trainData = "mnist_train_vectors.csv";
+
+    @Parameter(names = "-trainLabels")
+    public static String trainLabels = "mnist_train_labels.csv";
+
+    @Parameter(names = "-testData")
+    public static String testData = "mnist_test_vectors.csv";
+
+    @Parameter(names = "-testLabels")
+    public static String testLabels = "mnist_test_labels.csv";
+
+    @Parameter(names = "-seed")
+    public static int seed = 138;
+
+    public Settings() {}
+
+    public Settings(int[] architecture, double learningRate, int miniBatchSize, double momentum) {
+        this.architecture = architecture;
+        this.learningRate = learningRate;
+        this.miniBatchSize = miniBatchSize;
+        this.momentum = momentum;
+    }
+
+    /*
+    public Settings(int[] architecture, double learningRate, int miniBatchSize, double momentum, int epochs, int seed,
+                    String resourcesDir, String answers, String trainData, String trainLabels, String testData,
+                    String testLabels) {
+        this(architecture, learningRate, miniBatchSize, momentum);
+        this.epochs = epochs;
+        this.resourcesDir = resourcesDir;
+        this.seed = seed;
+        this.answers = answers;
+        this.trainData = trainData;
+        this.trainLabels = trainLabels;
+        this.testData = testData;
+        this.testLabels = testLabels;
+    }
+    */
+
+
+}
